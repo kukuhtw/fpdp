fd5697115bf86dc2c4aab86035ed3f73f2e02c13
KukuhTW <KukuhTW@gmail.com>
2026-09-23 07:16:04 +0700

    feat: Implement autolink functionality for URLs in About Me and Profile pages; enhance user experience by making links clickable

--- app/Core/View.php
+++ app/Core/View.php
@@ -22,4 +22,28 @@ final class View
         require $viewPath;
         return (string) ob_get_clean();
     }
+
+    /**
+     * Turn bare http(s) URLs inside already-escaped HTML into clickable links.
+     * Must be called on text that has already been through htmlspecialchars().
+     */
+    public static function autolink(string $escapedText): string
+    {
+        return preg_replace_callback(
+            '/https?:\/\/[^\s<]+/i',
+            static function (array $match): string {
+                $url = $match[0];
+                $trail = '';
+                while ($url !== '' && str_contains('.,!?:)]}\'"', substr($url, -1))) {
+                    $trail = substr($url, -1) . $trail;
+                    $url = substr($url, 0, -1);
+                }
+                if ($url === '') {
+                    return $match[0];
+                }
+                return '<a href="' . $url . '" target="_blank" rel="noopener noreferrer nofollow ugc">' . $url . '</a>' . $trail;
+            },
+            $escapedText
+        ) ?? $escapedText;
+    }
 }
